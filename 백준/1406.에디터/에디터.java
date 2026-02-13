3a194cbbac28f9982989bab8548e234058588469
Jeyong <dlwpdyd201@naver.com>
2026-02-13 16:50:55 +0900

    feat: add 1406.에디터 solution

--- 백준/1406.에디터/에디터.java
+++ 백준/1406.에디터/에디터.java
@@ -1,5 +1,67 @@
-public class Main {
-    public static void main(String[] args) throws Exception {
-        // TODO: solve
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+class Main {
+
+    private static Integer M;
+    private static BufferedReader br;
+    private static Stack<Character> stack;
+
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        String line = br.readLine();
+        M = Integer.parseInt(br.readLine());
+
+        stack = init(line);
+
+        executeCommand();
+
+        System.out.println(stack.toString().replace("[", "").replace("]", "").replace(", ", ""));
+    }
+
+    private static void executeCommand() throws IOException {
+        Stack<Character> tmpStack = new Stack<>();
+        for (int i = 0; i < M; i++) {
+            StringTokenizer line = new StringTokenizer(br.readLine());
+            String command = line.nextToken();
+            switch (command) {
+                case "L" :
+                    if (!stack.isEmpty()) {
+                        Character pop = stack.pop();
+                        tmpStack.add(pop);
+                    }
+                    break;
+
+                case "D" :
+                    if (!tmpStack.isEmpty()) {
+                        stack.push(tmpStack.pop());
+                    }
+                    break;
+
+                case "B" :
+                    if (!stack.isEmpty()) {
+                        stack.pop();
+                    }
+                    break;
+
+                case "P" :
+                    String next = line.nextToken();
+                    stack.push(next.charAt(0));
+                    break;
+            }
+        }
+        while (!tmpStack.isEmpty()) {
+            stack.push(tmpStack.pop());
+        }
+    }
+
+    private static Stack<Character> init(String line) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : line.toCharArray()) {
+            stack.push(c);
+        }
+        return stack;
     }
 }
