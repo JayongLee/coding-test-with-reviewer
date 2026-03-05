118c6b26c729d73076c49f808a35bd102d90b5d9
Jeyong <dlwpdyd201@naver.com>
2026-03-06 01:54:58 +0900

    boj: add 1629.곱셈 solution & metadata

--- 백준/1629.곱셈/곱셈.java
+++ 백준/1629.곱셈/곱셈.java
@@ -1,26 +1,31 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
-public class Main {
-    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    private static long A, B, C;
+class Main {
+  static long C;
+  
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
 
-    public static void main(String[] args) throws IOException {
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        A = Long.parseLong(st.nextToken());
-        B = Long.parseLong(st.nextToken());
-        C = Long.parseLong(st.nextToken());
+    long A = Long.parseLong(st.nextToken());
+    long B = Long.parseLong(st.nextToken());
+    C = Long.parseLong(st.nextToken());
 
-        System.out.println(pow(A, B, C));
+    System.out.println(pow(A, B));
+  }
+
+  static long pow(long a, long exponent) {
+    if (exponent == 1) {
+      return a % C;
     }
+    
+    long temp = pow(a, exponent / 2);
 
-    private static long pow(long a, long b, long mod) {
-        if (b == 1) return a % mod;
-        long val = pow(a, b/2, mod);
-        val = val * val % mod;
-        if (b % 2 == 0) return val;
-        return val * a % mod;
+    if (exponent % 2 == 1) {
+      return (temp * temp % C) * (a % C) % C;
     }
+        
+    return temp * temp % C;
+  }
 }
