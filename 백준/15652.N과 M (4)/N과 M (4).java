8c2d5c3468f230721609f08480ce4fbc0998aee6
Jeyong <dlwpdyd201@naver.com>
2026-03-13 19:51:39 +0900

    boj: add 15652.N과 M (4) solution & metadata

---
+++ 백준/15652.N과 M (4)/N과 M (4).java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    private static int N, M;
+    private static int[] seq;
+    private static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        sb = new StringBuilder();
+        seq = new int[M];
+
+        backtrack(0);
+        System.out.println(sb);
+    }
+
+    private static void backtrack(int count) {
+        if (count == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(seq[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = 1; i <= N; i++) {
+            if (count > 0 && i < seq[count - 1]) continue;
+            seq[count] = i;
+            backtrack(count + 1);
+        }
+    }
+}
